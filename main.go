b596f4fa68ed63eea26056e5f67136732b6fc299
AbhinavRana <vishuchoudhary131@gmail.com>
2024-10-08 22:07:32 +0530

    basic structure and main file

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"kenshi/config"
+	"kenshi/database"
+	"kenshi/handlers"
+	"kenshi/middleware"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := config.New()
+
+	// Connect to MongoDB
+	client, err := database.ConnectMongoDB(cfg.MongoURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(nil)
+
+	// Create Fiber app
+	app := fiber.New()
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Next()
+	})
+
+	// Routes
+	api := app.Group("/api")
+
+	// User routes
+	api.Post("/signup", handlers.SignUp)
+	api.Post("/signin", handlers.SignIn)
+	api.Post("/signout", handlers.SignOut)
+
+	// Task routes (protected)
+	tasks := api.Group("/tasks", middleware.AuthRequired())
+	tasks.Post("/", handlers.CreateTask)
+	tasks.Get("/", handlers.GetAllTasks)
+	tasks.Get("/:id", handlers.GetTask)
+	tasks.Put("/:id", handlers.UpdateTask)
+	tasks.Delete("/:id", handlers.DeleteTask)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(app.Listen(":" + port))
+}
